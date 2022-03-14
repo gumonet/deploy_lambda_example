098349ceb71362fd7ec0212d3be127c4a6a37593
Angel Gutierrez <gutierrez.monola@gmail.com>
2022-03-14 17:03:57 -0600

    first commit

---
+++ source/handler.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    // TODO implement
+    //Comment from github
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
